9cd6dded33d12d2c262d81a5077984da6004f969
Eric Limjoco <ericlimjoco@gmail.com>
2022-02-19 13:38:26 -0500

    Remove 'npm install' from merge-deploy github action; add user table with delete function

--- src/components/UserManager.vue
+++ src/components/UserManager.vue
@@ -7,20 +7,30 @@
       </div>
 
       <button type="submit">
-        Create a User
+        Create User
       </button>
     </form>
 
     <h3>User List</h3>
-    <ul>
-      <li v-for="user in users" :key="user.id">
-        {{ user }}
-      </li>
-    </ul>
+    <table v-if="users.length">
+      <thead>
+        <td>Id</td>
+        <td>Name</td>
+        <td>Delete</td>
+      </thead>
+      <tr v-for="user in users" :key="user.id">
+        <td>{{ user.id }}</td>
+        <td>{{ user.name }}</td>
+        <td>
+          <button @click="onDelete(user.id)"> Delete </button>
+        </td>
+      </tr>
+    </table>
+    <span v-else>No users</span>
   </div>
 </template>
 <script>
-import { getUsers, createUser } from '../firebase'
+import { getUsers, createUser, deleteUser } from '../firebase'
 
 export default {
   data () {
@@ -32,13 +42,43 @@ export default {
     }
   },
   async beforeMount () {
-    this.users = await getUsers()
+    await this.updateUsers()
   },
   methods: {
+    async updateUsers () {
+      this.users = await getUsers()
+    },
     async onSubmit () {
       await createUser({...this.form})
       this.form.name = ''
+      this.updateUsers()
+    },
+    onDelete (id) {
+      deleteUser(id)
+      this.updateUsers()
     }
   }
 }
 </script>
+<style scoped>
+form {
+  margin-bottom: 3rem;
+}
+button {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+.form-group {
+  margin-bottom: 1rem;
+}
+table {
+  border-collapse: collapse;
+  margin: 0 auto;
+}
+td {
+  border: 1px solid slategray;
+  padding: 0.5rem;
+}
+thead td {
+  font-weight: 600;
+}
+</style>
